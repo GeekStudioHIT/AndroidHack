6064ac3e51c9dcbfeb0b1b3f7779b3643e12f56c
koinhug@126.com <koinhug@126.com>
2016-08-15 15:35:41 +0800

    LogTest

---
+++ Log/LogTest1/app/src/main/java/com/xorange/logtest1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.xorange.logtest1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            Process process = Runtime.getRuntime().exec("logcat -d");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            StringBuilder log=new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                log.append(line);
+            }
+            TextView tv = (TextView)findViewById(R.id.textView1);
+            tv.setText(log.toString());
+        } catch (IOException e) {
+        }
+
+    }
+}
